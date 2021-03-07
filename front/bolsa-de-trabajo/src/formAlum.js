654c7b300cb96559267d4cbcb30a4f7f9942b2f5
matotec <matias.chaile.1988@gmail.com>
2021-03-07 06:04:31 -0300

    form alumnos completo sin validaciones ni envio a la bbdd

--- front/bolsa-de-trabajo/src/formAlum.js
+++ front/bolsa-de-trabajo/src/formAlum.js
@@ -2,6 +2,10 @@ import React, { useState, useEffect } from 'react';
 import Button from 'react-bootstrap/Button';
 import {useHistory} from 'react-router-dom';
 import HeaderToInicio from './components/headerToInicio';    
+import NavDropdown from 'react-bootstrap/NavDropdown';
+import Form from 'react-bootstrap/Form'
+import './styles/formAlum.css';
+
 
 const FormAlumn = () => {
     
@@ -16,8 +20,42 @@ const FormAlumn = () => {
             <input name="nombre" type="text" placeholder="ingrese su nombre"  />
         </div>
         <div>
-          <label>Apellido</label>
+          <label>Apellidos</label>
             <input name="apellido" type="text" placeholder="ingrese su apellido"  />
+        </div>
+        <div>
+            <label>Tipo</label>
+                <div>
+                    <label>Dni</label>
+                    <input type="checkbox" value="" id="checkbox" name="dni"/>
+                    <label>Pasaporte</label>
+                    <input type="checkbox" value="" id="checkbox" name="dni"/>
+                    <label>Cedula</label>
+                    <input type="checkbox" value="" id="checkbox" name="dni"/>
+                </div>     
+        </div>
+        <div>
+            <label>Fecha De Nacimiento</label>
+            <input type="date" name="fechaNacimiento"></input>
+        </div>
+        <div>
+        <label>Email</label>
+            <input name="email" type="email" placeholder="ingrese su email"  />
+        </div>
+        <div>
+          <label>Carrera</label>
+            <input name="carrera" type="text" placeholder="ingrese su carrera"  />
+        </div>
+        <div>
+            <label>Fecha De Inicio de Carrera</label>
+            <input type="date" name="fechaDeInicioCarrera"></input>
+        </div>
+        <div>
+        <Form.Label>Ingrese su experiencia</Form.Label>
+        <Form.Control as="textarea" rows={4} id="tamañoTextArea"/>
+        </div>
+        <div>
+        <Button variant="secondary" >Enviar</Button>
         </div>
       </form>
       </>
